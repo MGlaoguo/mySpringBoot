411f43d4987de7e91a56463b0f9d0dd69db952ae
MGlaoguo <2456273850@qq.com>
2018-06-28 16:33:53 +0800

    StringUtilCode

--- common/src/main/java/utils/StringUtil.java
+++ common/src/main/java/utils/StringUtil.java
@@ -4,4 +4,42 @@ package utils;
  * @author guojiabin
  */
 public class StringUtil {
+    /**
+     *
+     * @param object object
+     * @return boolean
+     */
+    public static boolean isBlank(Object object){
+        if (object == null) {
+            return true;
+        }
+        if(object instanceof String){
+            CharSequence cs = (CharSequence) object;
+            int strLen;
+            if ((strLen = cs.length()) != 0) {
+                for(int i = 0; i < strLen; ++i) {
+                    if (!Character.isWhitespace(cs.charAt(i))) {
+                        return false;
+                    }
+                }
+                return true;
+            } else {
+                return true;
+            }
+        }
+        return true;
+    }
+    public static boolean isNotBlank(Object object){
+        return !isBlank(object);
+    }
+
+    public static void main(String[] args) {
+        String a="                    ";
+        char c = ' ';
+        System.out.println(isBlank(a));
+        System.out.println(isNotBlank(a));
+        System.out.println(isBlank(c));
+        System.out.println(isNotBlank(c));
+
+    }
 }
